5ed6086c8970db38342f8f1e8d8cac0fa7ba2683
Snowky92 <amaurypinheiro@hotmail.fr>
2024-09-26 14:45:45 +0200

    commit projet

--- lib/biblio_track.dart
+++ lib/biblio_track.dart
@@ -1,6 +1,46 @@
 import 'dart:async';
 
+import 'package:biblio_track/library.dart';
+
 import 'book.dart';
-// Fonction pour gérer un Stream d'emprunt de livres
+
 Stream<String> borrowStream(Book book) async* {
+  yield 'Demande d\'emprunt du livre "${book.title}" en cours...';
+  if (!book.isAvailable) {
+    yield 'Le livre "${book.title}" n\'est pas disponible.';
+    return;
+  }
+
+  await Future.delayed(Duration(seconds: 2));
+  book.isAvailable = false;
+  book.log('emprunté le livre');
+  book.log('ne rien faire');
+  yield 'Le livre "${book.title}" a été emprunté avec succès.';
+}
+
+void main() async {
+
+  Library<Book> library = Library<Book>();
+
+  Book book1 = Book(title: 'AHSOKA', author: 'E.K.Johnson');
+  Book book2 = Book(title: 'A song of fire and ice', author: 'George R.R Martins');
+  Book book3 = Book(title: 'Harry Potter', author: 'J.K Rowling');
+
+  library.add(book1);
+  library.add(book2);
+  library.add(book3);
+
+  library.showAvailableBooks();
+
+  await for (String message in borrowStream(book1)) {
+    print(message);
+  }
+
+  library.showAvailableBooks();
+
+  await book1.returnBook();
+
+  book1.showHistory();
+
+  library.showAvailableBooks();
 }
