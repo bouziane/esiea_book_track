3d2ce99b02c13f306fb4decf3a3535438f4fa203
Imad Saleem <105483840+ImadSlm@users.noreply.github.com>
2024-09-26 17:50:14 +0200

    Refactor user_test.dart and add unit test for book borrowing

--- test/user_test.dart
+++ test/user_test.dart
@@ -1,33 +1,15 @@
 import '../lib/book.dart';
 import '../lib/user.dart';
+import 'package:test/test.dart';
 
-void main() async {
-  void userHasBook(bool state, bool expected, String message) {
-    if (state != expected) {
-      print('Error: $message\nExpected: $expected\nActual: $state');
-    }
-  }
-
-  void bookAvailable(List<Book> userList, Book book, String message) {
-    if (!userList.contains(book)) {
-      print('Error: $message\nList does not contain: $book');
-    }
-  }
-
-  void unitTests() async {
+void main() {
+  test('User should be able to borrow a book', () async {
     User user = User('Test User');
     Book book = Book('Book', 'Author', true, []);
 
     await user.borrowBook(book);
 
-    userHasBook(book.isAvailable, false, 'Book should not be available');
-    bookAvailable(user.borrowedBooks, book, 'User should have borrowed Book');
-  }
-
-  try {
-    unitTests();
-    print('Successful');
-  } catch (e) {
-    print(e);
-  }
+    expect(book.isAvailable, false);
+    expect(user.borrowedBooks.length, 1);
+  });
 }
