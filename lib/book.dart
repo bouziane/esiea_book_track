dd5996dbaf1a218a84ce1a4533b159ecb63b8685
Jean-BaptisteDAULAUS <162100867+Jean-BaptisteDAULAUS@users.noreply.github.com>
2024-09-26 14:13:06 +0200

    Step1

--- lib/book.dart
+++ lib/book.dart
@@ -3,12 +3,35 @@ import 'package:biblio_track/history.dart';
 
 class Book with History {
   //TODO: Ajouter ici les propriétés d'un livre
+  String title;
+  String? author;
+  bool isAvailable;
+  List<String> history = [];
+
+  Book({
+    required this.title,
+    this.isAvailable = true
+  });
 
   // Méthode asynchrone pour emprunter un livre
-  Future<void> borrow() async {}
+  Future<void> borrow() async {
+    await Future.delayed(Duration(seconds: 2));
+  }
 
   // Méthode asynchrone pour retourner un livre
-  Future<void> returnBook() async {}
+  Future<void> returnBook() async {
+    await Future.delayed(Duration(seconds: 2));
+  }
+
+  @override
+  void log(String action) {
+    this.history.add(action);
+  }
+
+  @override
+  void showHistory() {
+    print(this.history);
+  }
 }
 
 extension UpperCaseTitle on Book {}
