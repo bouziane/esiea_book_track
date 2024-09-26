0b4c7ddbb097ffc06eda7208f233f049b708c701
Imad Saleem <105483840+ImadSlm@users.noreply.github.com>
2024-09-26 16:16:34 +0200

    Refactor borrowStream function and add User class for book borrowing

---
+++ lib/user.dart
@@ -0,0 +1,37 @@
+import 'book.dart';
+
+class User {
+  String name;
+  List<Book> borrowedBooks = [];
+
+  User(this.name);
+
+  Future<void> borrowBook(Book book) async {
+    if (borrowedBooks.length >= 3) {
+      print('$name cannot borrow more than 3 books.');
+      return;
+    }
+    if (book.isAvailable) {
+      await book.borrow();
+      borrowedBooks.add(book);
+    } else {
+      print('${book.title} is not available');
+    }
+  }
+
+  Future<void> returnBook(Book book) async {
+    if (borrowedBooks.contains(book)) {
+      await book.returnBook();
+      borrowedBooks.remove(book);
+    } else {
+      print('$name does not have ${book.title}');
+    }
+  }
+
+  void showBorrowedBooks() {
+    print('Borrowed books by $name :');
+    for (var book in borrowedBooks) {
+      print('- ${book.title}');
+    }
+  }
+}
